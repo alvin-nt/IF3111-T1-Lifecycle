c2e457bc7b104332d36f2e860f2f11505ccedcb9
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2015-02-08 11:44:24 +0700

    Tutorial 2: 3/4

--- T2-Intents/intents-implicit-browserfilter/src/main/java/com/github/alvin_nt/intents_implicit/BrowserActivity.java
+++ T2-Intents/intents-implicit-browserfilter/src/main/java/com/github/alvin_nt/intents_implicit/BrowserActivity.java
@@ -7,10 +7,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.webkit.WebView;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 
@@ -22,13 +20,13 @@ public class BrowserActivity extends ActionBarActivity {
         
         // allow network access in the UI thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                .permitNetwork().build();
+                .permitAll().build();
         StrictMode.setThreadPolicy(policy);
         
         setContentView(R.layout.activity_browser);
         
         Intent intent = getIntent();
-        TextView text = (TextView) findViewById(R.id.textView);
+        WebView web = (WebView) findViewById(R.id.webView);
         
         String action = intent.getAction();
         if(!action.equals(Intent.ACTION_VIEW)) {
@@ -37,17 +35,15 @@ public class BrowserActivity extends ActionBarActivity {
         
         // get the data
         Uri data = intent.getData();
-        URL url;
-        try {
-            url = new URL(data.getScheme(), data.getHost(), data.getPath());
-            BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line = "";
-            while((line = rd.readLine()) != null) {
-                text.append(line);
+        if(data != null) {
+            URL url;
+            try {
+                url = new URL(data.getScheme(), data.getHost(), data.getPath());
+                web.loadUrl(url.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
